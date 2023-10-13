e8510b28100f48004b678a40c033d8392f75e4be
Henry Peeples <hgpeeples48@gmail.com>
2023-10-12 21:10:24 -0500

    setup initial rust backend api server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/analytics", get(analytics_handler));
+
+    println!("Starting API server...");
+    // run it
+    axum::Server::bind(&"127.0.0.1:5174".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn analytics_handler() {
+    println!("API working");
 }
