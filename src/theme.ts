db983c2f8ca4105a416b6a2266840fb70ca12a86
Henry Peeples <hgpeeples48@gmail.com>
2023-10-14 15:33:37 -0500

    update theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -7,23 +7,26 @@ export type Theme = {
   primary: string;
   accent: string;
   background: string;
+  background2: string;
   text: string;
   secondary: string;
 };
 
 export const themes: { [key: string]: Theme } = {
   default: {
-    primary: "#FFA72A",
-    accent: "#458622",
-    background: "#FFC777",
-    text: "#504f4d",
-    secondary: "#7F5315",
+    primary: "#FE4A49",
+    accent: "#1B998B",
+    background: "#eee",
+    background2: "#fff",
+    text: "#1f1f1f",
+    secondary: "#39393A",
   },
   dark: {
-    primary: "#FFA72A",
-    accent: "#458622",
+    primary: "#FE4A49",
+    accent: "#1B998B",
     background: "#504f4d",
-    text: "#FFC777",
+    background2: "#2f2f2f",
+    text: "#fff",
     secondary: "#252525",
   },
 };
